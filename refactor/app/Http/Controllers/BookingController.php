5b508558cf40877a76801e658b9acc65e0855301
Raymond Usbal <raymond@philippinedev.com>
2017-03-12 03:36:34 +0800

    Refactored code adding functions: adminOrSuperAdmin(), getFromRequest(), convertTrueToYesNo()

--- refactor/app/Http/Controllers/BookingController.php
+++ refactor/app/Http/Controllers/BookingController.php
@@ -40,7 +40,7 @@ class BookingController extends Controller
             $response = $this->repository->getUsersJobs($user_id);
 
         }
-        elseif($request->__authenticatedUser->user_type == env('ADMIN_ROLE_ID') || $request->__authenticatedUser->user_type == env('SUPERADMIN_ROLE_ID'))
+        elseif($this->adminOrSuperAdmin($request))
         {
             $response = $this->repository->getAll($request);
         }
@@ -93,7 +93,6 @@ class BookingController extends Controller
      */
     public function immediateJobEmail(Request $request)
     {
-        $adminSenderEmail = config('app.adminemail');
         $data = $request->all();
 
         $response = $this->repository->storeJobEmail($data);
@@ -194,61 +193,35 @@ class BookingController extends Controller
 
     public function distanceFeed(Request $request)
     {
-        $data = $request->all();
+        $distance     = $this->getFromRequest($request, 'distance');
+        $time         = $this->getFromRequest($request, 'time');
+        $jobid        = $this->getFromRequest($request, 'jobid');
+        $session      = $this->getFromRequest($request, 'session_time');
+        $admincomment = $this->getFromRequest($request, 'admincomment');
 
-        if (isset($data['distance']) && $data['distance'] != "") {
-            $distance = $data['distance'];
-        } else {
-            $distance = "";
-        }
-        if (isset($data['time']) && $data['time'] != "") {
-            $time = $data['time'];
-        } else {
-            $time = "";
-        }
-        if (isset($data['jobid']) && $data['jobid'] != "") {
-            $jobid = $data['jobid'];
+        $flagged          = $this->convertTrueToYesNo($request, 'flagged');
+        $manually_handled = $this->convertTrueToYesNo($request, 'manually_handled');
+        $by_admin         = $this->convertTrueToYesNo($request, 'by_admin');
+
+        if ($flagged == 'yes' && $admincomment == '') {
+            return "Please, add comment";
         }
 
-        if (isset($data['session_time']) && $data['session_time'] != "") {
-            $session = $data['session_time'];
-        } else {
-            $session = "";
-        }
-
-        if ($data['flagged'] == 'true') {
-            if($data['admincomment'] == '') return "Please, add comment";
-            $flagged = 'yes';
-        } else {
-            $flagged = 'no';
-        }
-        
-        if ($data['manually_handled'] == 'true') {
-            $manually_handled = 'yes';
-        } else {
-            $manually_handled = 'no';
-        }
-
-        if ($data['by_admin'] == 'true') {
-            $by_admin = 'yes';
-        } else {
-            $by_admin = 'no';
-        }
-
-        if (isset($data['admincomment']) && $data['admincomment'] != "") {
-            $admincomment = $data['admincomment'];
-        } else {
-            $admincomment = "";
-        }
         if ($time || $distance) {
-
-            $affectedRows = Distance::where('job_id', '=', $jobid)->update(array('distance' => $distance, 'time' => $time));
+            $affectedRows = Distance::where('job_id', '=', $jobid)->update([
+                'distance' => $distance, 
+                'time' => $time,
+            ]);
         }
 
-        if ($admincomment || $session || $flagged || $manually_handled || $by_admin) {
-
-            $affectedRows1 = Job::where('id', '=', $jobid)->update(array('admin_comments' => $admincomment, 'flagged' => $flagged, 'session_time' => $session, 'manually_handled' => $manually_handled, 'by_admin' => $by_admin));
-
+        if ($admincomment || $session) {
+            $affectedRows1 = Job::where('id', '=', $jobid)->update([
+                'admin_comments'   => $admincomment, 
+                'flagged'          => $flagged, 
+                'session_time'     => $session, 
+                'manually_handled' => $manually_handled, 
+                'by_admin'         => $by_admin,
+            ]);
         }
 
         return response('Record updated!');
--- refactor/app/Http/Controllers/Controller.php
+++ refactor/app/Http/Controllers/Controller.php
@@ -11,4 +11,20 @@ use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 class Controller extends BaseController
 {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+
+    protected function adminOrSuperAdmin($request) 
+    {
+        return $request->__authenticatedUser->user_type == env('ADMIN_ROLE_ID') 
+            || $request->__authenticatedUser->user_type == env('SUPERADMIN_ROLE_ID');
+    }
+
+    protected function getFromRequest(Request $request, $key, $default = "")
+    {
+        return $request->get($key) ?: $default;
+    }
+
+    protected function convertTrueToYesNo(Request $request, $key, $default = 'no')
+    {
+        return $request->get($key) == 'true' ? 'yes' : $default;
+    }
 }
